bd8410af58d18ab85e0a33ef4777a10236daa3db
leoff00 <leof0180@gmail.com>
2023-07-21 18:20:07 -0300

    fix: adjusting go routines recreation

--- ui/containers/meter.go
+++ ui/containers/meter.go
@@ -2,51 +2,103 @@ package containers
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"github.com/leoff00/gofastypo/db"
 )
 
 var (
-	wordCount           = 0
-	wpm                 = widget.NewLabel("")
-	wordsPerMinuteLabel string
+	mu                     sync.Mutex
+	startTime              time.Time
+	ticker                 = time.NewTicker(time.Second)
+	sharedGoroutineStarted bool
+	storedMetric           float64
+	visualMetric           string
+	wordCount              = 0
+	data                   = []string{"No metrics yet"}
+	metricView             = widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("No Metrics Yet...")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i])
+		})
 )
 
-func StartTyping(entry *widget.Entry) {
-	go func() {
-		startTime := time.Now()
-		for Duration >= 0 {
-			time.Sleep(time.Second)
-			entryText := entry.Text
-			words := strings.Fields(entryText)
-			wordCount = len(words)
-			wordsPerMinuteLabel = fmt.Sprintf("%.0f words/minute", calculateWordsPerMinute(&wordCount, startTime))
-			wpm.SetText(wordsPerMinuteLabel)
-		}
-	}()
+func parseMetric(m []db.MetricModel) []string {
+	var res []string
+	for i, v := range m {
+		res = append(res, fmt.Sprintf("wpm #%d: %.0f words/minute", (i+1), v.Metric))
+	}
+	return res
+}
 
+func calculateWordsPerMinute(wordCount int, startTime time.Time) float64 {
+	elapsedMin := time.Since(startTime).Minutes()
+	if elapsedMin > 0 {
+		wordsPerMinute := float64(wordCount) / elapsedMin
+		return wordsPerMinute
+	}
+	return 0
+}
+
+//! NEED TO THINK IN ONE SOLUTION TO FIX THE STORED METRIC
+//! AT START START TYPING FUNCTION.
+
+func StartTyping(entry *widget.Entry) {
+	mu.Lock()
+	if !sharedGoroutineStarted {
+		sharedGoroutineStarted = true
+		mu.Unlock()
+		startTime = time.Now()
+
+		go func() {
+			for range ticker.C {
+				mu.Lock()
+				words := strings.Fields(entry.Text)
+				wordCount = len(words)
+				time.Sleep(time.Second)
+
+				storedMetric = calculateWordsPerMinute(wordCount, startTime)
+				visualMetric = strconv.Itoa(int(storedMetric))
+				fmt.Println(storedMetric)
+				fmt.Println(startTime)
+				mu.Unlock()
+
+				if Duration <= 0 {
+					break
+				}
+			}
+		}()
+	} else {
+		mu.Unlock()
+	}
 }
 
 func StopTyping(txtArea *widget.Entry) {
-	wordsPerMinuteLabel = "0"
-	defaultMsg := fmt.Sprintf("%s words/minute", wordsPerMinuteLabel)
-	wpm.SetText(defaultMsg)
-	txtArea.SetText("")
+	ticker.Stop()
+	v, _ := strconv.Atoi(visualMetric)
+	go db.PersistData(float64(v))
+	data = parseMetric(db.GetData())
+	defer txtArea.SetText("")
 	txtArea.Disable()
 	txtArea.FocusGained()
 }
 
-func calculateWordsPerMinute(wordCount *int, startTime time.Time) float64 {
-	elapsedTime := time.Since(startTime).Minutes()
-	wordsPerMinute := float64(*wordCount) / elapsedTime
-	return wordsPerMinute
-}
-
 func MeterContainer() *fyne.Container {
-	return container.New(layout.NewCenterLayout(), wpm)
+	metricHeader := widget.NewLabel("Your metrics:")
+	savedMetrics := container.NewVScroll(metricView)
+	metricBox := container.NewVBox(metricHeader, savedMetrics)
+	rows := container.New(layout.NewGridLayoutWithRows(1), metricBox)
+	return container.NewCenter(rows)
 }
